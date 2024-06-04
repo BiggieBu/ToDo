f6db210dfabe87be32bd78c6d7313f680ab656c9
BiggieBu <parvyoy@gmail.com>
2024-06-04 10:33:15 +0530

    Initial Commit

---
+++ routes/auth.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oidc');
+const db = require('../db');
+var router = express.Router();
+
+passport.use(new GoogleStrategy({
+    clientID: process.env['CLIENT_ID'],
+    clientSecret: process.env['CLIENT_SECRET'],
+    callbackURL: '/oauth2/redirect/google',
+    scope: ['profile']
+}, function verify(issuer, profile, cb) {
+    db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
+        issuer,
+        profile.id
+    ], function (err, row) {
+        if (err) { return cb(err); }
+        if (!row) {
+            db.run('INSERT INTO users (name) VALUES (?)', [
+                profile.displayName
+            ], function (err) {
+                if (err) { return cb(err); }
+
+                var id = this.lastID;
+                db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
+                    id,
+                    issuer,
+                    profile.id
+                ], function (err) {
+                    if (err) { return cb(err); }
+                    var user = {
+                        id: id,
+                        name: profile.displayName
+                    };
+                    return cb(null, user);
+                });
+            });
+        } else {
+            db.get('SELECT * FROM users WHERE id = ?', [row.user_id], function (err, row) {
+                if (err) { return cb(err); }
+                if (!row) { return cb(null, false); }
+                return cb(null, row);
+            });
+        }
+    });
+}));
+
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        cb(null, { id: user.id, username: user.username })
+    });
+});
+
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
+
+router.get('/login', function (req, res, next) {
+    res.render('login');
+});
+
+router.get('/login/federated/google', passport.authenticate('google'));
+router.get('/oauth2/redirect/google', passport.authenticate('google', {
+    successRedirect: '/',
+    failuteRedirect: '/login'
+}))
+
+router.post('/logout', function (req, res, next) {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
+
+module.exports = router;
